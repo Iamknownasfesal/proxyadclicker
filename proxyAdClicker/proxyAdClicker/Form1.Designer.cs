7c6d3893df84f8442352c30635b00ae74bb4a79c
Fesal <39006465+Iamknownasfesal@users.noreply.github.com>
2020-02-22 11:30:14 +0300

    Update Form1.Designer.cs

--- proxyAdClicker/proxyAdClicker/Form1.Designer.cs
+++ proxyAdClicker/proxyAdClicker/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace proxyClickerSerious
+namespace proxyClickerSerious
 {
     partial class Form1
     {
@@ -200,7 +200,6 @@
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Proxy Ad Clicker";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
